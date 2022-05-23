af8930a3cc9c0cc5af0bb2d48d814a686f64fccc
darylsong <darylsong@tencent.com>
2022-05-23 10:30:13 +0800

    完成demo

--- src/main.js
+++ src/main.js
@@ -19,11 +19,11 @@ const mainWork = () => {
 mainWork()
 
 // 模拟react不卡顿主进程的方式
-// setInterval(() => {
-//     performWorkUntilDeadline()
-// }, 2000)
+setInterval(() => {
+    performWorkUntilDeadline()
+}, 2000)
 
 //卡顿模式
-setInterval(() => {
-    taskModule.taskQueue.forEach(i => handleWork(i))
-}, 2000)
+// setInterval(() => {
+//     taskModule.taskQueue.forEach(i => handleWork(i))
+// }, 2000)
